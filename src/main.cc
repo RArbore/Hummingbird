db91e1ace7fff2abd52414c3d9266f041ea6ad8f
Russel Arbore <russel.jma@gmail.com>
2022-02-14 21:30:18 -0600

    Minimal GL/GLFW example

--- src/main.cc
+++ src/main.cc
@@ -3,6 +3,8 @@
 #include <GLFW/glfw3.h>
 #include <GL/gl.h>
 
+static int width, height;
+
 int main(int argc, char **argv) {
 
     // TODO Parse command line args
@@ -12,15 +14,24 @@ int main(int argc, char **argv) {
 	return -1;
     }
 
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
     GLFWwindow* window = glfwCreateWindow(800, 600, "Hummingbird", NULL, NULL);
     if (!window) {
-	std::cerr << "ERROR: Couldn't initialize GLFW. Aborting." << std::endl;
+	std::cerr << "ERROR: Couldn't create window. Aborting." << std::endl;
 	glfwTerminate();
 	return -1;
     }
 
+    glfwShowWindow(window);
+    glfwMakeContextCurrent(window);
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+
     while (true);
 
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
