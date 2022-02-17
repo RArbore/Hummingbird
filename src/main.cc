5192ae885c30f8734cb2f239e817c54f23397065
cs79 <cs79@illinois.edu>
2022-02-16 21:59:38 -0600

    Formatting

--- src/main.cc
+++ src/main.cc
@@ -1,39 +1,39 @@
 #include <iostream>
 
-#include <GLFW/glfw3.h>
 #include <GL/gl.h>
+#include <GLFW/glfw3.h>
 
 static int width, height;
 
 int main(int argc, char **argv) {
 
-    // TODO Parse command line args
-    
-    if (!glfwInit()) {
-	std::cerr << "ERROR: Couldn't initialize GLFW. Aborting." << std::endl;
-	return -1;
-    }
+  // TODO Parse command line args
 
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Hummingbird", NULL, NULL);
-    if (!window) {
-	std::cerr << "ERROR: Couldn't create window. Aborting." << std::endl;
-	glfwTerminate();
-	return -1;
-    }
+  if (!glfwInit()) {
+    std::cerr << "ERROR: Couldn't initialize GLFW. Aborting." << std::endl;
+    return -1;
+  }
 
-    glfwShowWindow(window);
-    glfwMakeContextCurrent(window);
-    glfwGetFramebufferSize(window, &width, &height);
-    glViewport(0, 0, width, height);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwWaitEvents(); 
-    }
-
-    glfwDestroyWindow(window);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+  GLFWwindow *window = glfwCreateWindow(800, 600, "Hummingbird", NULL, NULL);
+  if (!window) {
+    std::cerr << "ERROR: Couldn't create window. Aborting." << std::endl;
     glfwTerminate();
-    return 0;
+    return -1;
+  }
+
+  glfwShowWindow(window);
+  glfwMakeContextCurrent(window);
+  glfwGetFramebufferSize(window, &width, &height);
+  glViewport(0, 0, width, height);
+  glClear(GL_COLOR_BUFFER_BIT);
+  glfwSwapBuffers(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwWaitEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return 0;
 }
