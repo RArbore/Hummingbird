9405a1ca4d8e2ea53f1354713693f2a7225276e6
cs79 <cs79@illinois.edu>
2022-02-21 20:37:23 -0600

    Config struct tests

--- tests/cli_tests.cc
+++ tests/cli_tests.cc
@@ -4,4 +4,46 @@
 
 #include "../include/cli.h"
 
-TEST_CASE("Test") { REQUIRE(true == true); }
+#include <string>
+
+TEST_CASE("Config constructor general case", "[cli]") {
+  char file_name[]{"test.json"};
+  Config cfg(file_name);
+  REQUIRE(strncmp(file_name, "test.json", 20) == 0);
+}
+
+TEST_CASE("Config constructor empty file name", "[cli]") {
+  char file_name[]{};
+  Config cfg(file_name);
+  REQUIRE(strncmp(file_name, "", 2) == 0);
+}
+
+TEST_CASE("Initialize general case", "[cli]") {
+  char file_name[]{"tests/cli_jsons/general.json"};
+  Config cfg(file_name);
+
+  REQUIRE(cfg.initialize() == 0);
+  REQUIRE(cfg.grav_constant == 1.0f);
+  REQUIRE(cfg.num_bodies == 10);
+}
+
+TEST_CASE("Initialize without gravity field", "[cli]") {
+  char file_name[]{"tests/cli_jsons/no_gravity.json"};
+  Config cfg(file_name);
+
+  REQUIRE(cfg.initialize() == -1);
+}
+
+TEST_CASE("Initialize without bodies field", "[cli]") {
+  char file_name[]{"tests/cli_jsons/no_bodies.json"};
+  Config cfg(file_name);
+
+  REQUIRE(cfg.initialize() == -1);
+}
+
+TEST_CASE("Initialize without fields", "[cli]") {
+  char file_name[]{"tests/cli_jsons/nothing.json"};
+  Config cfg(file_name);
+
+  REQUIRE(cfg.initialize() == -1);
+}
