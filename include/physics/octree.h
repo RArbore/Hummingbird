882c3bdcfe6c7e71c4ab22503db3d4863ba98f43
Russel Arbore <russel.jma@gmail.com>
2022-03-04 16:02:07 -0600

    Octree insert

--- include/physics/octree.h
+++ include/physics/octree.h
@@ -21,22 +21,44 @@
 static constexpr unsigned int C_MAX_DEPTH = 5; 
 static constexpr unsigned int C_NODE_SIZE = 8;
 
+struct AABB {
+  float x1, x2, y1, y2, z1, z2;
+};
+
+bool intersects(const AABB& aabb1, const AABB& aabb2);
+
+template<unsigned int x, unsigned int y, unsigned int z>
+__attribute__((always_inline))
+inline AABB get_sub_aabb(const AABB& aabb) {
+  return {
+    x ? 0.5f * (aabb.x1 + aabb.x2) : aabb.x1, x ? aabb.x2 : 0.5f * (aabb.x1 + aabb.x2),
+    y ? 0.5f * (aabb.y1 + aabb.y2) : aabb.y1, y ? aabb.y2 : 0.5f * (aabb.y1 + aabb.y2),
+    z ? 0.5f * (aabb.z1 + aabb.z2) : aabb.z1, z ? aabb.z2 : 0.5f * (aabb.z1 + aabb.z2)
+  };
+}
+
 /**
  * @brief Class to efficiently find potential collisions
  * 
  */
 class Octree {
 public:
-  explicit Octree(unsigned int max_depth_i); 
+  explicit Octree(const unsigned int max_depth_i, const AABB& aabb); 
+
+  void insert(const unsigned int to_store, const AABB& aabb);
 
 private:
-  struct OctreeNode {
+  struct Node {
+    Node(): bodies{0, 0, 0, 0, 0, 0, 0, 0}, num_stored(0), first_child(0) {}
     unsigned int bodies[C_NODE_SIZE]; 
-    unsigned int stored;
+    unsigned int num_stored;
     unsigned int first_child;
     bool is_leaf();
   }; 
 
-  std::vector<OctreeNode> nodes;
+  void insert(const unsigned int to_store, const AABB& aabb, const unsigned int root, const AABB& node_aabb);
+
+  std::vector<Node> nodes;
   size_t max_depth; 
+  AABB root_bound;
 };
--- src/physics/octree.cc
+++ src/physics/octree.cc
@@ -12,8 +12,38 @@
 
 #include <physics/octree.h>
 
-Octree::Octree(unsigned int max_depth_i): max_depth(max_depth_i) {}
+bool intersects(const AABB& aabb1, const AABB& aabb2) {
+  return aabb1.x1 <= aabb2.x2 && aabb2.x1 <= aabb1.x2
+    && aabb1.y1 <= aabb2.y2 && aabb2.y1 <= aabb1.y2
+    && aabb1.z1 <= aabb2.z2 && aabb2.z1 <= aabb1.z2;
+}
 
-bool Octree::OctreeNode::is_leaf() {
+Octree::Octree(const unsigned int max_depth_i, const AABB& aabb): max_depth(max_depth_i), root_bound(aabb) {}
+
+void Octree::insert(const unsigned int to_store, const AABB& aabb) {
+  insert(to_store, aabb, 0, root_bound);
+}
+
+void Octree::insert(const unsigned int to_store, const AABB& aabb, const unsigned int root, const AABB& node_aabb) {
+  auto& node = nodes.at(root);
+  if (node.num_stored < C_MAX_DEPTH) node.bodies[node.num_stored++] = to_store;
+  else {
+    if (!node.first_child) {
+      node.first_child = static_cast<unsigned int>(nodes.size());
+      for (unsigned int i = 0; i < 8; ++i) nodes.emplace_back();
+    }
+    AABB sub_aabb;
+    if (intersects(aabb, sub_aabb = get_sub_aabb<false, false, false>(node_aabb))) insert(to_store, aabb, node.first_child, sub_aabb);
+    if (intersects(aabb, sub_aabb = get_sub_aabb<true, false, false>(node_aabb))) insert(to_store, aabb, node.first_child + 1, sub_aabb);
+    if (intersects(aabb, sub_aabb = get_sub_aabb<false, true, false>(node_aabb))) insert(to_store, aabb, node.first_child + 2, sub_aabb);
+    if (intersects(aabb, sub_aabb = get_sub_aabb<true, true, false>(node_aabb))) insert(to_store, aabb, node.first_child + 3, sub_aabb);
+    if (intersects(aabb, sub_aabb = get_sub_aabb<false, false, true>(node_aabb))) insert(to_store, aabb, node.first_child + 4, sub_aabb);
+    if (intersects(aabb, sub_aabb = get_sub_aabb<true, false, true>(node_aabb))) insert(to_store, aabb, node.first_child + 5, sub_aabb);
+    if (intersects(aabb, sub_aabb = get_sub_aabb<false, true, true>(node_aabb))) insert(to_store, aabb, node.first_child + 6, sub_aabb);
+    if (intersects(aabb, sub_aabb = get_sub_aabb<true, true, true>(node_aabb))) insert(to_store, aabb, node.first_child + 7, sub_aabb);
+  }
+}
+
+bool Octree::Node::is_leaf() {
   return !first_child;
 }
